7fbbcc8b3020b74be3db4bb57355b5bb4109b258
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-08-05 15:15:03 +0000

    Creates and return an asyncio.Task for the wait_random coroutine

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,8 +6,10 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int) -> asyncio.Task:
+async def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
     creates and returns an asyncio.Task for the wait_random coroutine
     """
-    return asyncio.create_task(wait_random(max_delay))
+    task = asyncio.create_task(wait_random(max_delay))
+
+    return task
