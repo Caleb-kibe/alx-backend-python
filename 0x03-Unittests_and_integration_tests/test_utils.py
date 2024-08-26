ed1957d59cf81faa979725c5b38ed4c4f97e4bf1
caleb kibe <kiplagatcaleb2@gmail.com>
2024-08-26 15:06:16 +0300

    parameterized unittest for utils.access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,10 +16,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 1}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-
     """Tests `access_nested_map`'s output."""
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
